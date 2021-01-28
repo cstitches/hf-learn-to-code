8002af1359f93825ae3f2f27f09bdbd6fec57364
Caryn Dudley <cedudley@protonmail.com>
2021-01-28 09:32:20 -0800

    Add calculation to convert dog age to human years in Dog Age Calculator exercise

--- ch02/dogcalc.py
+++ ch02/dogcalc.py
@@ -1,2 +1,16 @@
+# solicit user's input for dog's name and age, return values stored as variables
+
 dog_name = input("What is your dog's name? ")
 dog_age = input("What is your dog's age? ")
+
+
+
+# calculate dog's age in "human years"
+
+    # unexpected result -- dog_age is treated as a string, not a number, replicates the string 7 times
+    # human_age = dog_age * 7
+    # need to convert string to integer
+
+human_age = int(dog_age) * 7
+
+
