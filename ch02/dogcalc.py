f2127ec03b2ad5c9b8be477468ebee84a67c3f47
Caryn Dudley <cedudley@protonmail.com>
2021-01-28 11:53:55 -0800

    Add output to user in Dog Age Calculator exercise

--- ch02/dogcalc.py
+++ ch02/dogcalc.py
@@ -1,11 +1,13 @@
-# solicit user's input for dog's name and age, return values stored as variables
+# Part 1 & 2: solicit user's input for dog's name and age, return values stored as variables
 
 dog_name = input("What is your dog's name? ")
+    # the preferred answer is "Dogmeat"
+
 dog_age = input("What is your dog's age? ")
 
 
 
-# calculate dog's age in "human years"
+# Part 3: calculate dog's age in "human years"
 
     # unexpected result -- dog_age is treated as a string, not a number, replicates the string 7 times
     # human_age = dog_age * 7
@@ -14,3 +16,17 @@ dog_age = input("What is your dog's age? ")
 human_age = int(dog_age) * 7
 
 
+# Part 4: output/print to user the dog's age in "human years"
+# Convert human_age integer back to string for printing
+
+    # Old method, concatenate the output together
+    # print("Your dog " + dog_name + " is " + str(human_age) + " in human years.")
+
+# Better method, pass multiple arguments separated by commas to print function; automatically includes a space between each arg
+
+print( 'Your dog',
+       dog_name,
+       'is',
+       str(human_age),
+       'years old in human years.'
+    )
