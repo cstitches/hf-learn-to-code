26cd763eae1328b429abd18727c84352d23e3e6b
Caryn Dudley <cedudley@protonmail.com>
2021-02-02 12:03:11 -0800

    Add handling of invalid input to Shoushiling exercise, and add Color Guessing Game exercise, as well as README updates.

--- ch03/shoushiling.py
+++ ch03/shoushiling.py
@@ -1,67 +1,82 @@
+
+### Set up
+# import random functionality
+# establish winner variable
+
 import random
 
-
-### Establish winner variable as a string
-# will be 'computer', 'player' or 'tie'
 winner = ''
 
 
 ### Generate computer's choice
-# Computer randomly chooses between 3 options using a random integer
-# Then the choice is converted to a string for readability.
-# Why use randint and not random.choice with a list of the three options?
+# computer randomly chooses between 3 options using a random integer
+# then the choice is converted to a string for readability
 
 comp_choice = random.randint(1,3)
 
 if comp_choice == 1:
-    comp_choice = "rock"
+    comp_choice = 'rock'
 
 elif comp_choice == 2:
-    comp_choice = "paper"
+    comp_choice = 'paper'
 
 elif comp_choice == 3:
-    comp_choice = "scissors"
+    comp_choice = 'scissors'
 
 else:
-    print("The computer is confused. The computer hurts itself in confusion.")
+    print('The computer is confused. It hurts itself in confusion.')
 
-### For testing -- don't want player to see computer's choice
+
+### For testing computer's choice
+
 #print("The computer chooses", comp_choice)
 
 
 
 ### Get player's choice
 
-player_choice = input("What's your play? rock, paper, or scissors? ")
 
-print("You chose", player_choice, "and the computer chose", comp_choice)
+player_choice = input('What\'s your play? rock, paper, or scissors? ')
 
 
 ### Check for invalid choice
-# Need to make this loop back to input for player_choice
+# while the player's choice is NOT any valid option, reprompt
+# `and` but not `or` because... 
+
+
+while (player_choice != 'rock' and
+       player_choice != 'paper' and
+       player_choice != 'scissors'):
+    player_choice = input('Sorry, you need to choose rock, paper, or scissors. ')
 
 
 
-### Check for tie
-# test if the computer's and player's choices are the same
+### Game logic
+
+# test for tie
 
 if player_choice == comp_choice:
-    winner = "neither, it's a tie"
+    winner = 'tie'
 
-### Check for winner
-# only need to test for computer's wins; after ruling out ties, if computer doesn't win, player does.
+# test if computer wins, otherwise player wins
 
 elif comp_choice == 'rock' and player_choice == 'scissors':
-    winner = 'computer'
+    winner = 'Computer'
 
 elif comp_choice == 'paper' and player_choice == 'rock':
-    winner = 'computer'
+    winner = 'Computer'
 
 elif comp_choice == 'scissors' and player_choice == 'paper':
-    winner = 'computer'
+    winner = 'Computer'
 
 else:
-    winner = 'player'
+    winner = 'Player'
 
 
-print("The winner is...", winner)
+### Output the outcome
+# message if tie, message if winner
+
+if winner == 'tie':
+    print('We both chose', comp_choice, 'play again.')
+else:
+    print(winner, 'won. The computer chose', comp_choice, 'and you chose', player_choice + '.')
